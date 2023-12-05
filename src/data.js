23ce5ec1d0faa214d91c81dfc6b07cd12e3ac560
queuing4oranges <katjazenker@gmail.com>
2023-12-05 23:26:28 +0100

    first commit

---
+++ src/data.js
@@ -0,0 +1,36 @@
+import pic1 from "../src/assets/images/len01.PNG";
+import pic2 from "../src/assets/images/len02.PNG";
+import pic3 from "../src/assets/images/len03.PNG";
+import pic4 from "../src/assets/images/len04.PNG";
+import pic5 from "../src/assets/images/len05.PNG";
+import pic6 from "../src/assets/images/len06.PNG";
+import pic7 from "../src/assets/images/len07.PNG";
+import pic8 from "../src/assets/images/len08.PNG";
+import pic9 from "../src/assets/images/len09.PNG";
+import pic10 from "../src/assets/images/len10.PNG";
+import pic11 from "../src/assets/images/len11.PNG";
+
+export const specification = [
+	{
+		id: 1, 
+		pics: [pic1, pic2, pic3, pic4, pic5, pic6, pic7, pic8, pic9, pic10, pic11], 
+		title: ["IdeaPad 3i Gen 6 (17, Intel)"],
+		subtitle: ["Velká obrazovka. Vysoký výkon."],
+		description: ["Užijte si skvělou zábavu s IdeaPad 3i Gen 6 (17&quot; Intel). Může se pochlubit výkonem procesorů Intel® Core™ 11. generace spolu s inteligentními funkcemi, které snižují námahu očí a eliminují rušivé elementy u vás doma. Užijte si 17&quot; displej s rozlišením FullHD s výjimečně tenkými rámečky, který je perfektní pro hraní her a sledování videí."],
+		processor: ["Až 11. generace procesorů Intel® Core™ i7"],
+		os: ["Až Windows 10 Pro"],
+		display: ["Až 17&quot; displej s rozlišením FullHD (1920×1080) s technologií IPS, jasem 300 nitů, antireflexní úpravou, 72% NTSC"],
+		ram: ["Až 16GB DDR4"], 
+		battery: ["Až 6 hodin výdrže (MobileMark 2014)", "Až 10 hodin (přehrávání 1080p videí)"],
+		memory: ["Až 1TB HDD", "Až 128GB SSD PCIe M.2"],
+		graphics: ["Až NVIDIA® MX450 (pouze pro modely s i7)", "Integrovaná grafika Intel® Iris® Xe"], 
+		size: ["Od 19,9 mm × 399 mm × 274 mm"], 
+		weight: ["Od 2,2 kg"],
+		color: ["Arctic Grey", "Abyss Blue", "Sand"],
+		sound: ["2 x 1,5W reproduktory s technologií Dolby Audio™", "Dva mikrofony"], 
+		connection: ["Až WiFi 6 (2x2 802.11ax)", "Bluetooth® 5.0"], 
+		camera: ["1 Mpx", "Kryt kamery pro větší soukromí"],
+		port: ["USB-C 3.1 Gen 2", "USB-A 3.1 Gen 2", "USB-A 2.0", "HDMI", "Čtečka SD karet", "Kombinovaný port pro sluchátka a mikrofon"],
+		software: ["Lenovo Utility", "Lenovo Vantage", "McAfee® LiveSafe™", "Microsoft Office"]
+	}
+]
